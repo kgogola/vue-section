908297bceb276e0faa36533fb0719e60fc9a8b82
LaYatiX <gpiwosz@gmail.com>
2018-04-11 20:27:17 +0200

    Initial commit
    fix code style after adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    'jest/globals': true,
+    es6: true
+  },
+  extends: ['standard', 'plugin:jest/recommended'],
+  // required to lint *.vue files
+  plugins: [
+    'html',
+    'jest'
+  ],
+  // add your custom rules here
+  rules: {
+    'operator-linebreak': ['error', 'after']
+   },
+  globals: {}
+}
