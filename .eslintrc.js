de525755f618c13e6f1d93c3d1a9faeda3f5b7dc
kgogola <32526127+kgogola@users.noreply.github.com>
2018-04-11 23:58:19 +0200

    Some changes
    Search engine, page navigator, favourite book icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   env: {
     browser: true,
-    node: true,
-    'jest/globals': true,
-    es6: true
+    node: true
   },
-  extends: ['standard', 'plugin:jest/recommended'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html',
-    'jest'
+    'vue'
   ],
   // add your custom rules here
-  rules: {
-    'operator-linebreak': ['error', 'after']
-   },
-  globals: {}
+  rules: {}
 }
--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,55 +1,75 @@
 <template>
 <div>
       <h1>Książki</h1>
+<input type="text" v-model="search" placeholder="search books">
   <div class="books-container">
         <Book
-            v-for="book in books"
+            v-for="book in booksFiltered"
             :key="book.id"
             :img="book.img"
             :title="book.title"
             :description="book.description"
             :id="book.id"
         />
+        <!-- <Book
+            title="Miecz przeznaczenia"
+            author="Sapkowski"
+            desciption="Super mega książka no mówię Ci Heniek"
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7QrIDjxxaX9wrPevWBooVqRVV5Kj2iMEID5zfr3D1a4_orDYD"
+        />
+         -->
   </div>
 </div>
 
 </template>
 
 <script>
-import Book from '@/components/Book'
+import Book from '@/components/Book';
 export default {
   components: {
-    Book
+      Book
   },
-  asyncData () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          books: [
-            {
-              id: '1',
-              title: 'Wieża Jaskółki',
-              description: 'Super mega książka',
-              img: 'https://i.ytimg.com/vi/PgKISfwfbBw/maxresdefault.jpg'
-            },
-            {
-              id: '2',
-              title: 'Miecz przeznaczenia',
-              description: 'Super mega książka no mówię Ci Heniek',
-              img:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7QrIDjxxaX9wrPevWBooVqRVV5Kj2iMEID5zfr3D1a4_orDYD'
-            }
-          ]
-        })
-      }, 1500)
-    })
+    data () {
+      return {
+          search:""
+      }
+  },
+  computed: {
+     booksFiltered: function() {
+         return this.books.filter((book)=>{
+             return book.title.match(this.search)
+         })
+     } 
+  },
+  asyncData(){
+      return new Promise((resolve, reject) => {
+          setTimeout(()=>{
+              resolve({
+                books:[
+                    {
+                        id: "1",
+                        title: "Wieża Jaskółki", 
+                        description: "Super mega książka", 
+                        img: "https://i.ytimg.com/vi/PgKISfwfbBw/maxresdefault.jpg"
+                    },
+                    {
+                        id: "2",
+                        title: "Miecz przeznaczenia", 
+                        description: "Super mega książka no mówię Ci Heniek", 
+                        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7QrIDjxxaX9wrPevWBooVqRVV5Kj2iMEID5zfr3D1a4_orDYD"
+                    }
+                ]
+              })
+
+          }, 1500)
+      })
   }
 }
 </script>
 
 <style scoped>
-.books-container {
-  display: flex;
-}
+    .books-container{
+        display: flex;
+    }
 </style>
 
