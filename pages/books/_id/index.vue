783406383bec28444ec2fffa7c951be35170fbc0
Geecek <gicalamaciej@gmail.com>
2018-04-12 10:07:28 +0200

    Add books fetching from Google API

--- pages/books/_id/index.vue
+++ pages/books/_id/index.vue
@@ -11,30 +11,10 @@
 
 <script>
 export default {
-  asyncData (context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          book: [
-            {
-              id: '1',
-              title: 'Wieża Jaskółki',
-              author: 'Sapkowski',
-              description: 'Super mega książka',
-              img: 'https://i.ytimg.com/vi/PgKISfwfbBw/maxresdefault.jpg'
-            },
-            {
-              id: '2',
-              title: 'Miecz przeznaczenia',
-              author: 'Sapkowski',
-              description: 'Super mega książka no mówię Ci Heniek',
-              img:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7QrIDjxxaX9wrPevWBooVqRVV5Kj2iMEID5zfr3D1a4_orDYD'
-            }
-          ].find(el => el.id === context.params.id)
-        })
-      }, 1500)
-    })
+  computed: {
+    book () {
+      return this.$store.state.books[this.$route.params.id - 1]
+    }
   }
 }
 </script>
